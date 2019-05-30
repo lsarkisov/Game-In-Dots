795ca80b64f42ea9a5306831d82e7b1f63c64888
sarkisov.levon <sarkisov.levon@gmail.com>
2019-05-30 08:15:10 +0200

    refactored square.js

--- src/components/game-board/square.js
+++ src/components/game-board/square.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Square extends Component {
-  onClick = () => {
-    // console.log(this.props.status);
-  }
-
-  render() {
-    const { status } = this.props;
-    return (
-      <div className="game-board_square" onClick={this.onClick}>
-        { status }
-      </div>
-    );
-  }
-}
+const Square = ({ status }) => (
+  <div className="game-board_square">
+    { status }
+  </div>
+);
 
 Square.defaultProps = {
   status: null,
